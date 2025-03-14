1f3ff719f905e26f80b912a3cec7a0e35b4ecc1c
wcc007 <1034942735@qq.com>
2025-03-14 18:29:28 +0800

    save

--- src/webview/components/NodeTypes.tsx
+++ src/webview/components/NodeTypes.tsx
@@ -1,26 +1,102 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface NodeData {
   label: string;
 }
 
-export const FileNode: React.FC<{ data: NodeData }> = ({ data }) => (
-  <div className='file-node'>
-    <div className='node-content'>
-      <span className='node-icon'>📄</span>
-      <span>{data.label}</span>
-    </div>
-  </div>
-);
+export const FileNode: React.FC<{ data: NodeData }> = ({ data }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [label, setLabel] = useState(data.label);
+  const [showToolbar, setShowToolbar] = useState(false);
 
-export const FolderNode: React.FC<{ data: NodeData }> = ({ data }) => (
-  <div className='file-node'>
-    <div className='node-content'>
-      <span className='node-icon'>📁</span>
-      <span>{data.label}</span>
+  const handleDoubleClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleBlur = () => {
+    setIsEditing(false);
+    window.acquireVsCodeApi().postMessage({ type: 'renameNode', newName: label });
+  };
+
+  return (
+    <div
+      className='file-node'
+      onDoubleClick={handleDoubleClick}
+      onMouseEnter={() => setShowToolbar(true)}
+      onMouseLeave={() => setShowToolbar(false)}
+    >
+      {isEditing ? (
+        <input
+          type='text'
+          value={label}
+          onChange={(e) => setLabel(e.target.value)}
+          onBlur={handleBlur}
+          autoFocus
+        />
+      ) : (
+        <div className='node-content'>
+          <span className='node-icon'>📄</span>
+          <span>{label}</span>
+        </div>
+      )}
+      {showToolbar && (
+        <div className='node-toolbar'>
+          <button onClick={() => window.acquireVsCodeApi().postMessage({ type: 'addNode' })}>Add</button>
+          <button onClick={() => window.acquireVsCodeApi().postMessage({ type: 'deleteNode' })}>Delete</button>
+          <button onClick={() => window.acquireVsCodeApi().postMessage({ type: 'copyNode' })}>Copy</button>
+          <button onClick={() => window.acquireVsCodeApi().postMessage({ type: 'pasteNode' })}>Paste</button>
+        </div>
+      )}
     </div>
-  </div>
-);
+  );
+};
+
+export const FolderNode: React.FC<{ data: NodeData }> = ({ data }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [label, setLabel] = useState(data.label);
+  const [showToolbar, setShowToolbar] = useState(false);
+
+  const handleDoubleClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleBlur = () => {
+    setIsEditing(false);
+    window.acquireVsCodeApi().postMessage({ type: 'renameNode', newName: label });
+  };
+
+  return (
+    <div
+      className='file-node'
+      onDoubleClick={handleDoubleClick}
+      onMouseEnter={() => setShowToolbar(true)}
+      onMouseLeave={() => setShowToolbar(false)}
+    >
+      {isEditing ? (
+        <input
+          type='text'
+          value={label}
+          onChange={(e) => setLabel(e.target.value)}
+          onBlur={handleBlur}
+          autoFocus
+        />
+      ) : (
+        <div className='node-content'>
+          <span className='node-icon'>📁</span>
+          <span>{label}</span>
+        </div>
+      )}
+      {showToolbar && (
+        <div className='node-toolbar'>
+          <button onClick={() => window.acquireVsCodeApi().postMessage({ type: 'addNode' })}>Add</button>
+          <button onClick={() => window.acquireVsCodeApi().postMessage({ type: 'deleteNode' })}>Delete</button>
+          <button onClick={() => window.acquireVsCodeApi().postMessage({ type: 'copyNode' })}>Copy</button>
+          <button onClick={() => window.acquireVsCodeApi().postMessage({ type: 'pasteNode' })}>Paste</button>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export const nodeTypes = {
   file: FileNode,
